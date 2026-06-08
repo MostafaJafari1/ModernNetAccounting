8f7c84302e886e8962c64b6bea38952731dbc1ae
Mostafa Jafari <36862442+ArtisticProgramming@users.noreply.github.com>
2026-06-08 09:22:11 +0330

    feat(buildingblocks): add guard unit tests for domain guards

---
+++ src/buildingblocks/tests/BuildingBlocks.Domain.Tests/Guards/CollectionGuardTests.cs
@@ -0,0 +1,53 @@
+﻿using BuildingBlocks.Domain.Guards.Extensions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BuildingBlocks.Domain.Tests.Guards;
+
+public class CollectionGuardTests
+{
+    [Fact]
+    public void NotEmpty_ShouldThrowArgumentException_WhenCollectionIsNull()
+    {
+        IEnumerable<string>? myCollection = null;
+
+        var exception = Assert.Throws<ArgumentException>(() =>
+            Guard.Against.NotEmpty(myCollection!));
+
+        Assert.Contains("myCollection", exception.ParamName);
+    }
+
+    [Fact]
+    public void NotEmpty_ShouldThrowArgumentException_WhenCollectionIsEmpty()
+    {
+        var myCollection = new List<int>();
+
+        Assert.Throws<ArgumentException>(() =>
+            Guard.Against.NotEmpty(myCollection));
+    }
+
+    [Fact]
+    public void NotEmpty_ShouldReturnCollection_WhenCollectionHasElements()
+    {
+        var myCollection = new List<string> { "Test1", "Test2" };
+
+        var result = Guard.Against.NotEmpty(myCollection);
+
+        Assert.Same(myCollection, result);
+        Assert.Equal(2, result.Count());
+    }
+
+    [Fact]
+    public void NotEmpty_ShouldThrowExceptionWithCustomMessage_WhenProvided()
+    {
+        var myCollection = new List<int>();
+        var customMessage = "List must not be null";
+
+        var exception = Assert.Throws<ArgumentException>(() =>
+            Guard.Against.NotEmpty(myCollection, customMessage));
+
+        Assert.StartsWith(customMessage, exception.Message);
+        Assert.Contains("myCollection", exception.Message);
+    }
+}
---
+++ src/buildingblocks/tests/BuildingBlocks.Domain.Tests/Guards/GuidGuardExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BuildingBlocks.Domain.Guards.Extensions;
+using Xunit;
+
+namespace BuildingBlocks.Domain.Tests.Guards;
+
+public class GuidGuardTests
+{
+    [Fact]
+    public void NotEmpty_ShouldThrowArgumentException_WhenGuidIsEmpty()
+    {
+        Guid value = Guid.Empty;
+
+        Assert.Throws<ArgumentException>(() => Guard.Against.NotEmpty(value));
+    }
+
+    [Fact]
+    public void NotEmpty_ShouldReturnValue_WhenGuidIsNotEmpty()
+    {
+        Guid value = Guid.NewGuid();
+
+        var result = Guard.Against.NotEmpty(value);
+
+        Assert.Equal(value, result);
+    }
+}
