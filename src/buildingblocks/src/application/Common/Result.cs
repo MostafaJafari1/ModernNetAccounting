661c3a6c8f6b34d3f4e4273b1a73a2a04449db1c
Mostafa Jafari <36862442+ArtisticProgramming@users.noreply.github.com>
2026-06-08 19:06:19 +0330

    feat(application): add common abstractions for result handling

---
+++ src/buildingblocks/src/application/Common/Result.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Application.Common;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public string Message { get; }
+    public List<string> Errors { get; }
+    public T Data { get; }
+
+    private Result(bool isSuccess, T data, string message, List<string> errors)
+    {
+        IsSuccess = isSuccess;
+        Data = data;
+        Message = message;
+        Errors = errors ?? new();
+    }
+
+    public static Result<T> Success(T data, string message = null) =>
+        new(true, data, message, null);
+
+    public static Result<T> Failure(string error) =>
+        new(false, default, null, new List<string> { error });
+
+    public static Result<T> Failure(List<string> errors) =>
+        new(false, default, null, errors);
+}
---
+++ src/buildingblocks/src/application/Common/Unit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Application.Common;
+
+/// <summary>
+/// Using Unit as void type for Command with no return value, or Query with no result.
+/// </summary>
+public sealed record Unit
+{
+    public static readonly Unit Value = new();
+    private Unit() { }
+}
