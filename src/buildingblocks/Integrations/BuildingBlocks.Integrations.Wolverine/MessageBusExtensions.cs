0506d5807b1b125d6be97669f5abcd12815f1d9e
Mostafa Jafari <36862442+ArtisticProgramming@users.noreply.github.com>
2026-06-12 12:40:35 +0330

    add Wolverine integration and MessageBus extensions

---
+++ src/buildingblocks/Integrations/BuildingBlocks.Integrations.Wolverine/MessageBusExtensions.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.Application.Common;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wolverine;
+
+namespace BuildingBlocks.Integrations.Wolverine;
+public static class MessageBusExtensions
+{
+    public static async Task<Result<TResult>> SendQueryAsync<TResult>(
+        this IMessageBus bus,
+        object query)
+        => await bus.InvokeAsync<Result<TResult>>(query);
+
+    public static async Task<Result<TResult>> SendCommandAsync<TResult>(
+        this IMessageBus bus,
+        object command)
+        => await bus.InvokeAsync<Result<TResult>>(command);
+}
