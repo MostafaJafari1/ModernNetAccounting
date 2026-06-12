9c9740b9aaa2f8cad42795e0d692a578c849d186
Mostafa Jafari <36862442+ArtisticProgramming@users.noreply.github.com>
2026-06-12 18:10:20 +0330

    feat(buildingblocks): add Marten integration for event sourcing
    - Add MartenEventStoreRepository and IMartenEventStoreRepository
    - Add MartenUnitOfWork for transaction management
    - Add MartenExtensions and StreamArchivedEvent

---
+++ src/buildingblocks/Integrations/BuildingBlocks.Integration.Marten/Events/StreamArchivedEvent.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Integration.Marten.Events;
+
+// رویداد domain برای آرشیو - بخشی از مدل رویدادی شما
+public record StreamArchivedEvent(Guid StreamId, DateTimeOffset ArchivedAt);
---
+++ src/buildingblocks/Integrations/BuildingBlocks.Integration.Marten/Extensions/MartenExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Integration.Marten.Extensions
+{
+    internal class MartenExtensions
+    {
+    }
+}
