f54a6a3f6e960e3461bd86e79005edf0204722e2
Mostafa Jafari <36862442+ArtisticProgramming@users.noreply.github.com>
2026-06-14 12:02:54 +0330

    refactor(marten): update marten extensions structure and cleanup obsolete files

---
+++ src/buildingblocks/Integrations/BuildingBlocks.Integration.Marten/Extensions/MartenBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Marten;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Marten.MartenServiceCollectionExtensions;
+
+namespace BuildingBlocks.Integration.Marten.Extensions;
+
+public static class MartenBuilderExtensions
+{
+    public static MartenConfigurationExpression AddMartenDefaults(
+        this IServiceCollection services,
+        string connectionString,
+        Action<StoreOptions>? configure = null,
+        Action<MartenDefaults>? defaults = null)
+    {
+        // مقادیر پیش‌فرض
+        var martenDefaults = new MartenDefaults();
+        defaults?.Invoke(martenDefaults);
+
+        return services.AddMarten(options =>
+        {
+            options.Connection(connectionString);
+            options.AutoCreateSchemaObjects = martenDefaults.AutoCreate;
+            options.Events.StreamIdentity = martenDefaults.StreamIdentity;
+
+            configure?.Invoke(options);
+        });
+    }
+}
---
+++ src/buildingblocks/Integrations/BuildingBlocks.Integration.Marten/Extensions/MartenDefaults.cs
@@ -0,0 +1,13 @@
+﻿using JasperFx;
+using JasperFx.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Integration.Marten.Extensions;
+
+public class MartenDefaults
+{
+    public AutoCreate AutoCreate { get; set; } = AutoCreate.All;
+    public StreamIdentity StreamIdentity { get; set; } = StreamIdentity.AsGuid;
+}
