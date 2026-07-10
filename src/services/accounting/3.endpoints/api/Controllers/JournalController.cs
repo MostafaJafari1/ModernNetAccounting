56c69ffabde084e75a7253e82de6d7d5aa189ebe
Mostafa Jafari <36862442+ArtisticProgramming@users.noreply.github.com>
2026-07-10 12:30:32 +0330

    refactor: move common namespaces to GlobalUsings.cs
    Removed redundant using directives from controllers and Program.cs, and centralized them in GlobalUsings.cs.

--- src/services/accounting/3.endpoints/api/Controllers/JournalController.cs
+++ src/services/accounting/3.endpoints/api/Controllers/JournalController.cs
@@ -1,11 +1,4 @@
-﻿using Accounting.Core.RequestResponse.Journals.Commands;
-using BuildingBlocks.API.Controllers;
-using BuildingBlocks.Application.Common;
-using BuildingBlocks.Integrations.Wolverine;
-using Microsoft.AspNetCore.Mvc;
-using Wolverine;
-
-namespace Accounting.Endpoints.Api.Controllers
+﻿namespace Accounting.Endpoints.Api.Controllers
 {
     public class JournalController : BaseApiController
     {
@@ -18,5 +11,9 @@ namespace Accounting.Endpoints.Api.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateJournal([FromBody] CreateJournalCommand command)
          => Create(await _bus.SendCommandAsync<Unit>(command));
+
+        [HttpPost("{id:guid}/post")]
+        public async Task<IActionResult> PostJournal([FromRoute] Guid id)
+            => Create(await _bus.SendCommandAsync<Unit>(new PostJournalCommand { JournalEntryId = id }));
     }
 }
